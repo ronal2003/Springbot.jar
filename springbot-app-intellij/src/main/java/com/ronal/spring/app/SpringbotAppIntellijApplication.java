5cda431f33910b56dd78faeab319bcce1ce6693c
ronal2003 <ronuldox.2003@gmail.com>
2024-08-19 01:22:45 -0500

    primer proyecto con spring bot

---
+++ springbot-app-intellij/src/main/java/com/ronal/spring/app/SpringbotAppIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.ronal.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotAppIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotAppIntellijApplication.class, args);
+	}
+
+}
---
+++ springbot-app-intellij/src/test/java/com/ronal/spring/app/SpringbotAppIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronal.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbotAppIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
